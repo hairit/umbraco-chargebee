28741676811444e53332b8d9921203b43651bb20
Marcus <mpham@paritycorp.com>
2023-08-24 00:23:45 +0700

    Init project

---
+++ Utilities/Constants.cs
@@ -0,0 +1,20 @@
+﻿namespace Chargebee.Utilities
+{
+	public struct FieldType
+	{
+		public const string Textbox = "Textbox";
+		public const string Textarea = "Textarea";
+		public const string Checkbox = "Checkbox";
+		public const string Dropdown = "Dropdown";
+		public const string Date = "Date";
+		public const string DateTime = "Date/Time";
+		public const string Email = "Email";
+		public const string Phone = "Phone";
+	}
+
+	public struct ButtonType
+	{
+		public const string Submit = "Submit";
+		public const string Cancel = "Cancel";
+	}
+}
